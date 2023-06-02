1a5de500c8293391132c1bf566ae6992a1de3d62
Harrison King <Harrison.King@lowcarboncontracts.uk>
2023-06-02 16:38:20 +0100

    getPopularMovies() async function completed

--- js/script.ts
+++ js/script.ts
@@ -1,6 +1,7 @@
 const global = {
     currentPage: window.location.pathname
-};
+}
+
 async function getPopularMovies() {
     const { results } = await fetchData('movie/popular');
     results.forEach(movie => {
@@ -8,11 +9,13 @@ async function getPopularMovies() {
         div.classList.add('card');
         div.innerHTML = `
         <a href="movie-details.html?id=${movie.id}">
-            ${movie.poster_path ? `<img 
+            ${
+                movie.poster_path ? `<img 
                 src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
                 class="card-img-top" 
-                alt="${movie.title}">` :
-            `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}">`}
+                alt="${movie.title}">` : 
+                `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}">`
+            }
         </a>
         <div class="card-body">
         <h5 class="card-title">${movie.title}</h5>
@@ -23,18 +26,20 @@ async function getPopularMovies() {
         `;
         // Append card to movies div
         document.querySelector('#popular-movies').appendChild(div);
-    });
-}
-;
+    })
+};
+
 // Fetch data from TMDB API when given an endpoint
-async function fetchData(endpoint) {
+async function fetchData(endpoint: string) {
     const API_KEY = '6914b9f184c05b3e80d856880f362424';
     const API_URL = 'https://api.themoviedb.org/3/';
+
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await response.json();
+
     return data;
-}
-;
+};
+
 // Highlight Current Page
 function highlightCurrentPage() {
     const links = document.querySelectorAll('.nav-link');
@@ -42,9 +47,9 @@ function highlightCurrentPage() {
         if (link.getAttribute('href') == global.currentPage) {
             link.classList.add('active');
         }
-    });
-}
-;
+    })
+};
+
 // Init App
 function init() {
     switch (global.currentPage) {
@@ -64,6 +69,8 @@ function init() {
             console.log('Search');
             break;
     }
+
     highlightCurrentPage();
 }
+
 document.addEventListener('DOMContentLoaded', init);
